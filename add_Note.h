a34639488636fb127406dfd945d850c9bb40c072
Aashi Jaulim <aashi.jaulim@gmail.com>
2022-04-11 17:10:26 +0400

    Changed add_Note.h

--- add_Note.h
+++ add_Note.h
@@ -2,9 +2,9 @@
 #define ADDNOTE
 void addNote()
 {
-    FILE *ptr;
-    int j=40, yr, count=1;
-    char id[10], note[1000], *ti, *day, *date, *m, *x;
+    FILE *ptr, *ptr1;
+    int j=40, yr, count=1, exist;
+    char id[10], note[1000], *ti, *day, *date, *m, *x, NID[1000];
     time_t t=time(NULL);
     struct tm tm=*localtime(&t);
     yr=tm.tm_year+1900;
@@ -29,20 +29,38 @@ void addNote()
             ti=x;
         }
     }
-    ptr=fopen("personal_Diary.txt", "a");
+    ptr=fopen("personal_Diary.txt", "r");
     if(ptr==NULL)
     {
         printf("\nERROR!!!\nFile not found.");
     }
     else
     {
-        printf("\nEnter the note ID (in 3 digits): ");
-        scanf("%s", id);
+        do
+        {
+            exist=0;
+            printf("\nEnter the note ID (in 3 digits): ");
+            scanf("%s", id);
+            
+            while(!feof(ptr))
+            {
+                fgets(NID, sizeof(NID), ptr);
+                strtok(NID, "             ");
+                if(strcmp(NID, id)==0)
+                {
+                    exist=1;
+                    printf("\n\nThis note ID already exists.");
+                    break;
+                }
+            }
+        } while(exist==1);
+        fclose(ptr);
         printf("\nEnter the note: ");
         getchar();
         fgets(note, sizeof(note), stdin); 
-        fprintf(ptr,"%s             %s %s %s %d             %s           %s", id, day, date, m, yr, ti, note);
+        ptr1=fopen("personal_Diary.txt", "a");
+        fprintf(ptr1,"%s             %s %s %s %d             %s           %s", id, day, date, m, yr, ti, note);
+        fclose(ptr1);
     }
-    fclose(ptr);
 }
 #endif
