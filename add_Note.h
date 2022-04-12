0e86a8a5ceba1257963f3b140c475880c1718ba4
Aashi Jaulim <aashi.jaulim@gmail.com>
2022-04-12 16:30:26 +0400

    Updated add_Note.h

--- add_Note.h
+++ add_Note.h
@@ -2,8 +2,8 @@
 #define ADDNOTE
 void addNote()
 {
-    FILE *ptr, *ptr1;
-    int j=40, yr, count=1, exist;
+    FILE *ptr;
+    int j=160, yr, count=1, exist;
     char id[10], note[1000], *ti, *day, *date, *m, *x, NID[1000];
     time_t t=time(NULL);
     struct tm tm=*localtime(&t);
@@ -41,7 +41,6 @@ void addNote()
             exist=0;
             printf("\nEnter the note ID (in 3 digits): ");
             scanf("%s", id);
-            
             while(!feof(ptr))
             {
                 fgets(NID, sizeof(NID), ptr);
@@ -49,7 +48,7 @@ void addNote()
                 if(strcmp(NID, id)==0)
                 {
                     exist=1;
-                    printf("\n\nThis note ID already exists.");
+                    printf("\nThis note ID already exists.\n");
                     break;
                 }
             }
@@ -58,9 +57,34 @@ void addNote()
         printf("\nEnter the note: ");
         getchar();
         fgets(note, sizeof(note), stdin); 
-        ptr1=fopen("personal_Diary.txt", "a");
-        fprintf(ptr1,"%s             %s %s %s %d             %s           %s", id, day, date, m, yr, ti, note);
-        fclose(ptr1);
+        ptr=fopen("personal_Diary.txt", "a");
+        fprintf(ptr,"%s             %s %s %s %d             %s           %s", id, day, date, m, yr, ti, note);
+        fclose(ptr);
+        ptr=fopen("personalDiary.txt","a");
+        printf("\n\n\tINTERFACE OF THE DIARY AFTER ADDING A NEW NOTE :) \n\n");
+        for(int i=0;i<175;i++)
+        {
+            printf("\xB2");
+        }
+        printf("\n\xB2\xB2\n\xB2\xB2\t%s %s %s %d\n\xB2\xB2\t",day, date, m, yr);
+        printf("%s\n\xB2\xB2",ti);
+        printf("\n\xB2\xB2\tDEAR DIARY,\n\xB2\xB2\n\xB2\xB2\t");
+        for(int i=0;note[i]!='\0';i++)
+        {
+            printf("%c", note[i]);
+            if(i>j && (note[i]==33 || note[i]==46 || note[i]==32))
+            {  
+                printf("\n\xB2\xB2\t");
+                j=j+160;
+            }
+        }
+        printf("\xB2\xB2\n");
+        for(int i=0;i<175;i++)
+        {
+            printf("\xB2");
+        }
+        printf("\n");
+        fclose(ptr);
     }
 }
 #endif
