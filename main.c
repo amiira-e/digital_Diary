21ffacd44f0d5ab81a56fdd83298bf8a5b4f7d44
amiira <amiiraemambokus@gmail.com>
2022-04-13 16:52:23 +0400

    Added menu() to main.c

--- main.c
+++ main.c
@@ -4,16 +4,8 @@
 #include<conio.h>
 #include<time.h>
 #include<windows.h>
-#include"add_Note.h"
-#include"change_PW.h"
-#include"check_PW.h"
-#include"delete_Note.h"
-#include"view_Diary.h"
+#include"menu.h"
 void main()
 {
-    addNote();
-    //checkPW();
-    //changePW();
-    //deleteNote();
-    //viewDiary();
+   menu();
 }
